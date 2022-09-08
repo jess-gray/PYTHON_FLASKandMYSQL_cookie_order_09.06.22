5c39d3f1622ccebefe7f39a84ea6f8666e5c397c
jess_gray <jac.cooper32@gmail.com>
2022-09-08 19:05:43 -0400

    homeowrk completed with notes

--- flask_app/controllers/cookies.py
+++ flask_app/controllers/cookies.py
@@ -7,3 +7,42 @@ from flask_app.models.cookie import Cookie
 def route():
     all_orders = Cookie.get_all()
     return render_template('read_all.html', all_the_orders = all_orders)
+
+@app.route('/cookies/new') #this is the page/form to add a new order
+def user():
+    return render_template('create.html')
+
+@app.route('/new/order', methods = ["POST"]) #this is actually adding the new order
+def create_order():
+    print(request.form)
+    if not Cookie.validate_create(request.form): #this is validating info
+        return redirect ('/cookies/new')
+    data = {
+        'customer_name': request.form['customer_name'],
+        'cookie_type': request.form['cookie_type'],
+        'amount': request.form['amount']
+    }
+    show_user = Cookie.create(data)
+    return redirect('/cookies')
+
+@app.route('/cookies/edit/<int:id>') #this is the page/form to edit an order
+def update(id):
+    data = {
+        'id' : id
+    }
+    a_order = Cookie.get_one(data)
+    return render_template('edit.html', one_order = a_order)
+
+@app.route('/edit/<int:id>', methods = ["POST"]) #this is actually adding the new order
+def edit_order(id):
+    print(request.form)
+    if not Cookie.validate_create(request.form): #this is validating info
+        return redirect (f'/cookies/edit/{id}')
+    data = {
+        'id' : id,
+        'customer_name': request.form['customer_name'],
+        'cookie_type': request.form['cookie_type'],
+        'amount': request.form['amount']
+    }
+    Cookie.update(data)
+    return redirect('/cookies')
--- flask_app/models/cookie.py
+++ flask_app/models/cookie.py
@@ -1,7 +1,9 @@
 from flask_app.config.mysqlconnection import connectToMySQL
+from flask import flash
 
 class Cookie:
     def __init__(self,data):
+        self.id = data['id']
         self.customer_name = data['customer_name']
         self.cookie_type = data['cookie_type']
         self.amount = data['amount']
@@ -17,3 +19,55 @@ class Cookie:
         for one_order in results:
             all_orders.append(cls(one_order))
         return all_orders
+    
+    @classmethod #this is to read one orders info
+    def get_one(cls, data):
+        query = 'SELECT * FROM orders WHERE id = %(id)s;'
+        results = connectToMySQL('cookie_orders').query_db(query, data)
+        print(results)
+        return cls(results[0])
+
+    
+    @classmethod #this is actually adding a new order
+    def create(cls, data):
+        query = 'INSERT INTO orders (customer_name, cookie_type, amount) VALUES (%(customer_name)s, %(cookie_type)s, %(amount)s);'
+        results = connectToMySQL('cookie_orders').query_db(query, data)
+        print(results)
+        return results
+    
+    @classmethod #this is to actually edit the order
+    def update(cls, data):
+        query = 'UPDATE orders set customer_name = %(customer_name)s, cookie_type = %(cookie_type)s, amount = %(amount)s WHERE id = %(id)s;'
+        results = connectToMySQL('cookie_orders').query_db(query, data)
+        print(results)
+        return results
+    
+    @staticmethod #this is to validate new orders
+    def validate_create(reqForm):
+        is_valid = True
+        if len(reqForm['customer_name']) < 2:
+            flash('customer name is too short!')
+            is_valid = False
+        if len(reqForm['cookie_type']) < 2:
+            flash('cookie type is too short!')
+            is_valid = False
+        if int(reqForm['amount']) < 1: #need to add int for numbers
+            flash('must enter at least 1 box')
+            is_valid = False
+        return is_valid
+        #need to add amount
+    
+    @staticmethod #this is to validate edits 
+    def validate_edit(reqForm):
+        is_valid = True
+        if len(reqForm['customer_name']) < 0:
+            flash('must enter customer name!')
+            is_valid = False
+        if len(reqForm['cookie_type']) < 0:
+            flash('must enter cookie type!')
+            is_valid = False
+        if int(reqForm['amount']) < 1:
+            flash('must enter at least 1 box')
+            is_valid = False
+        return is_valid
+        #need to add amount
